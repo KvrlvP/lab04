c3f5ad6af0f54a4673c54a9c213649191236d3a6
Kvrlv <paolaz2005d@gmail.com>
2026-04-07 16:50:38 -0500

    añado  Checkbox

---
+++ app/src/main/java/com/example/lab04/CheckboxComponent.kt
@@ -0,0 +1,26 @@
+package com.example.lab04
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MyCheckbox() {
+    var checked by remember { mutableStateOf(false) }
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { checked = it }
+        )
+        Text(text = if (checked) "Marcado" else "Sin marcar")
+    }
+}
